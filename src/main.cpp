f789b2ad99869c3f46e1133edeaabf74d8c4aea6
Razu <149871034+Trid1on@users.noreply.github.com>
2023-12-30 17:40:23 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,4 @@
-#include <string>
 #include <iostream>
-
 #include "WW_Window/WW_Window.hpp"
 #include "Document/Document.hpp"
 #include "HTMLParser/HTMLParser.hpp"
@@ -12,74 +10,26 @@
 int main(int argc, char* argv[])
 {
     Loader loader;
-    std::wstring html;
+    const std::wstring html = (argc < 2) ? loader.load_from_file("../../tests/html_files/demo.html") : loader.load_from_file(std::string(argv[1]));
 
-    if (argc < 2)
-    {
-        std::cout << "Loading default demo." << std::endl << 
-            "Pass path to executable to load different file." << std::endl;
-        html = loader.load_from_file("../../tests/html_files/demo.html");
-    }
-
-    else
-        html = loader.load_from_file(std::string(argv[1]));
-
-    std::cout << "Loaded string" << std::endl;
-
-    if (html == L"")
+    if (html.empty())
         return -1;
 
     WW_Window window(WINDOW_WIDTH, WINDOW_HEIGHT);
-
     HTMLParser parser;
     Document document = parser.construct_document_from_string(html);
-
-    std::cout << "Constructed document" << std::endl;
 
     Layout layout;
     layout.set_width(WINDOW_WIDTH);
     layout.set_view(window.getView());
     layout.construct_from_document(document);
 
-    std::cout << "Constructed layout" << std::endl;
-
     while (window.isOpen())
     {
         sf::Event event;
-
         while (window.pollEvent(event))
         {
-            switch (event.type)
-            {
-                case sf::Event::Closed:
-                {
-                    window.close();
-                    break;
-                }
-
-                case sf::Event::KeyPressed:
-                {
-                    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
-                        window.close();
-                    break;
-                }
-
-                case sf::Event::Resized:
-                {
-                    sf::Vector2f size = 
-                        static_cast<sf::Vector2f>(window.getSize());
-                    sf::View view(sf::FloatRect(0.f, 0.f, size.x, size.y));
-                    window.setView(view);
-                    layout.set_view(view);
-                    layout.set_width(size.x);
-                    layout.update();
-
-                    break;
-                }
-
-                default:
-                    break;
-            }
+            handleEvent(event, window, layout);
         }
 
         window.load_from_layout(layout);
@@ -87,4 +37,38 @@ int main(int argc, char* argv[])
         window.show_layout();
         window.display();
     }
+
+    return 0;
+}
+
+void handleEvent(const sf::Event& event, WW_Window& window, Layout& layout)
+{
+    switch (event.type)
+    {
+        case sf::Event::Closed:
+            window.close();
+            break;
+
+        case sf::Event::KeyPressed:
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+                window.close();
+            break;
+
+        case sf::Event::Resized:
+            handleResizeEvent(window, layout);
+            break;
+
+        default:
+            break;
+    }
+}
+
+void handleResizeEvent(WW_Window& window, Layout& layout)
+{
+    sf::Vector2f size = static_cast<sf::Vector2f>(window.getSize());
+    sf::View view(sf::FloatRect(0.f, 0.f, size.x, size.y));
+    window.setView(view);
+    layout.set_view(view);
+    layout.set_width(size.x);
+    layout.update();
 }

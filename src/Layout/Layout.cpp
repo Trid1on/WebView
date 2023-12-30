a6dd6ae0913ab46c61c85f9695550707c9f89c55
Razu <149871034+Trid1on@users.noreply.github.com>
2023-12-30 17:37:39 +0200

    Update Layout.cpp

--- src/Layout/Layout.cpp
+++ src/Layout/Layout.cpp
@@ -1,11 +1,9 @@
 #include <boost/algorithm/string.hpp>
 #include <stack>
 #include <unordered_map>
+#include <SFML/Graphics.hpp>
 
 #include "Layout.hpp"
-
-#define PARAGRAPH_BREAK_PADDING 14
-#define LEADING 2
 
 namespace WordSizes
 {
@@ -13,139 +11,84 @@ namespace WordSizes
     std::unordered_map<std::wstring, int> word_heights;
 }
 
-int Layout::get_word_length(const std::wstring& word)
-{
-    int word_length;
-
-    if (WordSizes::word_lengths.find(word) == WordSizes::word_lengths.end())
-    {
-        text.setString(word);
-        word_length = text.getLocalBounds().width;
-        WordSizes::word_lengths.insert({word, word_length});
-    }
-
-    else
-        word_length = WordSizes::word_lengths.at(word);
-
-    return word_length;
-}
-
-int Layout::get_word_height(const std::wstring& word)
-{
-    int word_height;
-
-    if (WordSizes::word_heights.find(word) == WordSizes::word_heights.end())
-    {
-        text.setString(word);
-        word_height = text.getLocalBounds().height;
-        WordSizes::word_heights.insert({word, word_height});
-    }
-
-    else
-        word_height = WordSizes::word_heights.at(word);
-
-    return word_height;
-}
-
-
 Layout::Layout()
+    : cursor_x(0), cursor_y(0), is_font_set(font.loadFromFile("/usr/share/fonts/TTF/DejaVuSerif.ttf")),
+      text(), is_last_box_finalized(false)
 {
-    cursor_x = 0;
-    cursor_y = 0;
-    boxes = {};
-
-    is_font_set = font.loadFromFile("/usr/share/fonts/TTF/DejaVuSerif.ttf");
-
     text.setCharacterSize(14);
     text.setFont(font);
 
-    is_last_box_finalized = false;
+    initializeSpaceWordSizes();
+}
 
+void Layout::initializeSpaceWordSizes()
+{
     if (WordSizes::word_lengths.count(L" ") == 0)
     {
         text.setString(" ");
-        WordSizes::word_lengths.insert({L" ", text.getLocalBounds().width});
-        WordSizes::word_heights.insert({L" ", text.getLocalBounds().height});
+        WordSizes::word_lengths[L" "] = text.getLocalBounds().width;
+        WordSizes::word_heights[L" "] = text.getLocalBounds().height;
         text.setString("");
     }
-
 }
 
-void push_remaining_children(const std::vector<std::shared_ptr<HTMLElement>> &c, 
-        std::stack<std::vector<std::shared_ptr<HTMLElement>>> &r)
+int Layout::get_word_length(const std::wstring &word)
 {
-    if (c.size() <= 1)
-        r.push({});
+    return get_word_size(word).first;
+}
 
-    else
+int Layout::get_word_height(const std::wstring &word)
+{
+    return get_word_size(word).second;
+}
+
+std::pair<int, int> Layout::get_word_size(const std::wstring &word)
+{
+    auto &word_lengths = WordSizes::word_lengths;
+    auto &word_heights = WordSizes::word_heights;
+
+    if (word_lengths.find(word) == word_lengths.end())
     {
-        std::vector<std::shared_ptr<HTMLElement>> 
-            rest_children(c.begin() + 1, c.end());
+        text.setString(word);
+        int word_length = text.getLocalBounds().width;
+        int word_height = text.getLocalBounds().height;
 
-        r.push(rest_children);
+        word_lengths[word] = word_length;
+        word_heights[word] = word_height;
+
+        return {word_length, word_height};
     }
+
+    return {word_lengths.at(word), word_heights.at(word)};
 }
 
 void Layout::construct_from_document(const Document &doc)
 {
-    boxes = {};
+    boxes.clear();
     cursor_x = 0;
     cursor_y = 0;
-
     document = doc;
 
-    auto node = document.get_elements().front();
+    processDocumentNode(document.get_elements().front()->get_children());
+}
 
-    std::stack<std::vector<std::shared_ptr<HTMLElement>>> remaining_children;
-    auto root_children = node->get_children();
-
-    push_remaining_children(node->get_children(), remaining_children);
-
-    while (true)
+void Layout::processDocumentNode(const std::vector<std::shared_ptr<HTMLElement>> &children)
+{
+    for (const auto &node : children)
     {
         if (node->is_text_node())
             add_string(node->get_text());
-
-        else if (node->is_paragraph_node())
-        {
-            if (boxes.size() > 0)
-            {
-                is_last_box_finalized = true;
-                cursor_y += PARAGRAPH_BREAK_PADDING;
-            }
-        }
-
-        if (node->get_children().size() > 0)
-        {
-            node = node->get_children().front();
-            push_remaining_children(node->get_children(), remaining_children);
-        }
-
-        else
-        {
-            while (remaining_children.top().size() == 0)
-            {
-                remaining_children.pop();
-
-                if (remaining_children.size() == 0)
-                    break;
-            }
-
-            if (remaining_children.size() == 0)
-                break;
-
-            auto current_children = remaining_children.top();
-            remaining_children.pop();
-
-            node = current_children.front();
-            push_remaining_children(current_children, remaining_children);
-        }
+        else if (node->is_paragraph_node() && !boxes.empty())
+            finalizeCurrentBox();
+        else if (!node->get_children().empty())
+            processDocumentNode(node->get_children());
     }
 }
 
-void Layout::set_width(const int &new_width)
+void Layout::finalizeCurrentBox()
 {
-    width = new_width;
+    is_last_box_finalized = true;
+    cursor_y += PARAGRAPH_BREAK_PADDING;
 }
 
 void Layout::add_string(const std::wstring &string)
@@ -153,89 +96,52 @@ void Layout::add_string(const std::wstring &string)
     std::vector<std::wstring> words;
     boost::split(words, string, boost::is_any_of(L" "));
 
-    int space_length = WordSizes::word_lengths.at(L" ");
-    int space_height = WordSizes::word_heights.at(L" ");
-    int box_height = space_height;
-    int box_length = 0;
-    int word_length, word_height;
-
-    Box current_box;
-    if (boxes.size() > 0)
+    for (const auto &word : words)
     {
-        if (!is_last_box_finalized)
-        {
-            current_box = boxes.at(boxes.size() - 1);
-            boxes.pop_back();
-            box_length = current_box.get_width();
-        }
-
-        else
-            current_box.set_coordinates({cursor_x, cursor_y});
+        addWordToCurrentBox(word);
     }
+}
 
+void Layout::addWordToCurrentBox(const std::wstring &word)
+{
+    auto [word_length, word_height] = get_word_size(word);
+
+    if (!boxes.empty() && !is_last_box_finalized)
+    {
+        extendCurrentBox(word, word_length, word_height);
+    }
     else
     {
-        current_box.set_coordinates({cursor_x, cursor_y});
-        is_last_box_finalized = false;
+        startNewBox(word, word_length, word_height);
     }
+}
 
-    std::wstring current_string = current_box.get_box_string(); 
-    std::wstring old_string = current_string;
+void Layout::extendCurrentBox(const std::wstring &word, int word_length, int word_height)
+{
+    auto &current_box = boxes.back();
 
-    std::vector<int> coordinates = current_box.get_coordinates();
-
-    for (const auto& word : words)
+    if (fitsWithinWidth(current_box.get_width() + word_length))
     {
-        current_string += word;
-
-        word_length = get_word_length(word);
-        word_height = get_word_height(word);
-
-        if (cursor_x + box_length + space_length + word_length < width)
-        {
-            box_length += space_length + word_length;
-            if (word_height > box_height)
-                box_height = word_height;
-
-            current_box.set_box_string(current_string);
-        }
-
-        else
-        {
-            current_box.set_box_string(old_string);
-            current_box.set_width(box_length);
-            current_box.set_height(box_height);
-            boxes.push_back(current_box);
-            is_last_box_finalized = true;
-            cursor_y += box_height + LEADING;
-
-            current_box = Box();
-
-            box_length = word_length;
-            box_height = word_height;
-
-            current_string = word;
-            old_string = current_string;
-            current_box.set_box_string(current_string);
-            current_box.set_coordinates({cursor_x, cursor_y});
-        }
-
-        current_box.set_width(box_length);
-        current_box.set_height(box_height);
-
-        old_string = current_string;
-        current_string += L" ";
+        current_box.extendBox(word);
     }
-
-    // If we hit the last word, then we added an extra space to the length
-    if (!is_last_box_finalized)
+    else
     {
-        box_length -= space_length;
-        current_box.set_width(box_length);
+        finalizeCurrentBox();
+        startNewBox(word, word_length, word_height);
     }
+}
 
-    boxes.push_back(current_box);
-    cursor_y += box_height;
+bool Layout::fitsWithinWidth(int length) const
+{
+    return cursor_x + length <= width;
+}
+
+void Layout::startNewBox(const std::wstring &word, int word_length, int word_height)
+{
+    Box new_box(cursor_x, cursor_y);
+    new_box.extendBox(word);
+    boxes.push_back(new_box);
+    is_last_box_finalized = false;
 }
 
 std::vector<Box> Layout::get_boxes() const
@@ -245,28 +151,18 @@ std::vector<Box> Layout::get_boxes() const
 
 int Layout::get_max_width()
 {
-    int width = 0;
-
-    for (auto box : boxes)
-    {
-        if (box.get_width() > width)
-            width = box.get_width();
-    }
-    
-    return width;
+    return std::max_element(boxes.begin(), boxes.end(), [](const auto &a, const auto &b) {
+               return a.get_width() < b.get_width();
+           })->get_width();
 }
 
 int Layout::get_max_height()
 {
-    int height = 0;
-
-    for (auto box : boxes)
-    {
-        if (box.get_coordinates().at(1) + box.get_height() > height)
-            height = box.get_coordinates().at(1) + box.get_height();
-    }
-
-    return height;
+    return std::max_element(boxes.begin(), boxes.end(), [](const auto &a, const auto &b) {
+               return a.get_coordinates().at(1) + a.get_height() < b.get_coordinates().at(1) + b.get_height();
+           })->get_coordinates().at(1) + std::max_element(boxes.begin(), boxes.end(), [](const auto &a, const auto &b) {
+                                                          return a.get_height() < b.get_height();
+                                                      })->get_height();
 }
 
 void Layout::update()
@@ -275,15 +171,18 @@ void Layout::update()
 
     auto center = view.getCenter();
     auto size = view.getSize();
-    sf::FloatRect view_bounds(center.x - (size.x / 2), center.y - (size.y / 2),
-            size.x, size.y);
+    sf::FloatRect view_bounds(center.x - (size.x / 2), center.y - (size.y / 2), size.x, size.y);
 
-    for (auto& box : boxes)
+    for (auto &box : boxes)
     {
         auto coordinates = box.get_coordinates();
-        box.set_visible(view_bounds.intersects(sf::FloatRect(coordinates.at(0), 
-                        coordinates.at(1), box.get_width(), box.get_height())));
+        box.set_visible(view_bounds.intersects(sf::FloatRect(coordinates.at(0), coordinates.at(1), box.get_width(), box.get_height())));
     }
+}
+
+void Layout::set_width(const int &new_width)
+{
+    width = new_width;
 }
 
 void Layout::set_view(const sf::View &new_view)
